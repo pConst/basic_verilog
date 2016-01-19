2a3f6094280b64559647c835ec47a963746aeaff
Konstantin Pavlov (en) <pavlovconst@gmail.com>
2016-01-19 19:23:27 +0300

    "Enable" input in DeBounce module

--- DeBounce.v
+++ DeBounce.v
@@ -4,43 +4,58 @@
 //--------------------------------------------------------------------------------
 
 // INFO --------------------------------------------------------------------------------
-//  Работает по двум отсчетам (медленного) клока. 
-//  Переключается в оба состояния с задержкой на два (медленных) клока
+//  Работает по двум отсчетам на входе, которые могут быть, а могут и не быть периодическими
+//  Переключается в оба состояния с задержкой на три такта
 
 
 /* DeBounce DB1 (
 	.clk(),
 	.nrst(),
+	.en(),
 	.in(),
 	.out()
 	);
 defparam DB1.WIDTH = 1;*/
 
 
-module DeBounce(clk,nrst,in,out);
+module DeBounce(clk,nrst,en,in,out);
 
 input wire clk;
 input wire nrst;
+input wire en;
+
 input wire [(WIDTH-1):0] in;
 output wire [(WIDTH-1):0] out;   // also "present state"
 
 parameter WIDTH = 1;
 
-reg [(WIDTH-1):0] prev = 0;
+reg [(WIDTH-1):0] d1 = 0;
+reg [(WIDTH-1):0] d2 = 0;
 
 always @ (posedge clk) begin
 	if (~nrst) begin
-		prev <= 0;
+		d1[(WIDTH-1):0] <= 0;
+		d2[(WIDTH-1):0] <= 0;
 	end
 	else begin
-		prev[(WIDTH-1):0] <= in[(WIDTH-1):0];
-	end
+		if (en) begin
+			d1[(WIDTH-1):0] <= d2[(WIDTH-1):0];
+			d2[(WIDTH-1):0] <= in[(WIDTH-1):0];
+		end;	// if
+	end		// else
 end
 
-wire [(WIDTH-1):0] switch_hi = (prev[(WIDTH-1):0] & in[(WIDTH-1):0]);
-wire [(WIDTH-1):0] n_switch_lo = (prev[(WIDTH-1):0] | in[(WIDTH-1):0]);
-
-SetReset SR(clk,nrst,switch_hi[(WIDTH-1):0],~n_switch_lo[(WIDTH-1):0],out[(WIDTH-1):0]);
+wire [(WIDTH-1):0] switch_hi = (d2[(WIDTH-1):0] & d1[(WIDTH-1):0]);
+wire [(WIDTH-1):0] n_switch_lo = (d2[(WIDTH-1):0] | d1[(WIDTH-1):0]);
+    
+SetReset SR (
+	.clk(clk),
+	.nrst(nrst),
+	.s(switch_hi[(WIDTH-1):0]),
+	.r(~n_switch_lo[(WIDTH-1):0]),
+	.q(out[(WIDTH-1):0]),
+	.nq()
+	);
 defparam SR.WIDTH = WIDTH;
 
 endmodule
---
+++ DeBounce_tb.v
@@ -0,0 +1,90 @@
+//--------------------------------------------------------------------------------
+// Main_TB.v
+// Konstantin Pavlov, pavlovconst@gmail.com
+//--------------------------------------------------------------------------------
+
+// INFO --------------------------------------------------------------------------------
+//
+//
+
+`timescale 1ns / 1ps
+
+module Main_tb();
+
+reg clk200;
+initial begin
+        #0 clk200 = 1;
+        forever 
+            #2.5 clk200 = ~clk200;
+end
+
+reg rst;
+initial begin
+        #10.2 rst = 1;
+        #5 rst = 0;
+        //#10000;
+        forever begin
+            #9985 rst = ~rst;
+            #5 rst = ~rst;
+        end
+end
+wire nrst = ~rst;
+
+reg rst_once;
+initial begin       // initializing non-X data before PLL starts
+        #10.2 rst_once = 1;
+        #5 rst_once = 0;
+end
+initial begin
+        #510.2 rst_once = 1;    // PLL starts at 500ns, clock appears, so doing the reset for modules
+        #5 rst_once = 0;
+end
+wire nrst_once = ~rst_once;
+
+wire [31:0] DerivedClocks;
+ClkDivider CD1 (
+    .clk(clk200),
+    .nrst(nrst_once),
+    .out(DerivedClocks[31:0]));
+defparam CD1.WIDTH = 32;
+
+wire [31:0] E_DerivedClocks;
+EdgeDetect ED1 (
+    .clk(clk200),
+    .nrst(nrst_once),
+    .in(DerivedClocks[31:0]),
+    .rising(E_DerivedClocks[31:0]),
+    .falling(),
+    .both()
+    );
+defparam ED1.WIDTH = 32;
+
+wire [15:0] RandomNumber1;
+c_rand RNG1 (
+    .clk(clk200),
+    .rst(rst_once),
+    .reseed(1'b0),
+    .seed_val(DerivedClocks[31:0]),
+    .out(RandomNumber1[15:0]));
+
+reg start;
+initial begin
+        #100.2 start = 1;
+        #5 start = 0;
+end
+
+//=================================================
+
+wire den = RandomNumber1[1] && RandomNumber1[2];
+wire dbout;
+DeBounce DB1 (
+	.clk(clk200),
+	.nrst(nrst_once),
+	.en(den),
+	.in(RandomNumber1[0]),
+	.out(dbout)
+	);
+defparam DB1.WIDTH = 1;
+    
+endmodule
+	
