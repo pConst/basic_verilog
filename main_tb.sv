9c1670a4729b3a0e7feaea98a7bc4ceda2df7249
Konstantin Pavlov (fm) <pavlovconst@gmail.com>
2019-02-08 13:56:50 +0300

    Added sample asynchronous external device logic

--- main_tb.sv
+++ main_tb.sv
@@ -17,6 +17,14 @@ initial begin
     #2.5 clk200 = ~clk200;
 end
 
+// external device "asynchronous" clock
+logic clk33;
+initial begin
+  #0 clk33 = 1'b0;
+  forever
+    #15.151 clk33 = ~clk33;
+end
+
 logic rst;
 initial begin
   #0 rst = 1'b0;
@@ -49,14 +57,15 @@ assign nrst_once = ~rst_once;
 logic [31:0] DerivedClocks;
 clk_divider #(
   .WIDTH( 32 )
-) CD1 (
+) cd1 (
   .clk( clk200 ),
   .nrst( nrst_once ),
+  .ena( 1'b1 ),
   .out( DerivedClocks[31:0] )
 );
 
 logic [31:0] E_DerivedClocks;
-edge_detect ED1[31:0] (
+edge_detect ed1[31:0] (
   .clk( {32{clk200}} ),
   .nrst( {32{nrst_once}} ),
   .in( DerivedClocks[31:0] ),
@@ -66,7 +75,7 @@ edge_detect ED1[31:0] (
 );
 
 logic [15:0] RandomNumber1;
-c_rand RNG1 (
+c_rand rng1 (
   .clk( clk200 ),
   .rst( rst_once ),
   .reseed( 1'b0 ),
@@ -77,8 +86,8 @@ c_rand RNG1 (
 logic start;
 initial begin
   #0 start = 1'b0;
-  #100.2 start = 1'b1;
-  #5 start = 1'b0;
+  #100 start = 1'b1;
+  #20 start = 1'b0;
 end
 
 // Module under test ==========================================================
@@ -89,5 +98,39 @@ Main M (    // module under test
   rst_once,
   out1,out2   // for compiler not to remove logic
 );
+
+// emulating external divice ==================================================
+// that works asynchronously on clk33 clock
+
+reg [15:0] test_data = 16'b1010_1100_1100_1111;
+reg [7:0] adc1_seq_cntr = 0;
+always_ff @(posedge clk33) begin
+  if( adc1_seq_cntr[7:0]==0 && ~ADC1_nCONV ) begin
+    ADC1_BUSY <= 1'b1;
+    ADC1_SDOUT <= test_data[15];
+    test_data[15:0] <= {test_data[14:0],1'b0};
+    adc1_seq_cntr[7:0] <= 1;
+  end
+
+  if( adc1_seq_cntr[7:0]>0 && adc1_seq_cntr[7:0]<33 && ADC1_SCLKOUT) begin
+    ADC1_SCLKOUT <= ~ADC1_SCLKOUT;
+    // emulating adc1 data
+    ADC1_SDOUT <= test_data[15];
+    test_data[15:0] <= {test_data[14:0],1'b0};
+    adc1_seq_cntr[7:0] <= adc1_seq_cntr[7:0] + 1'b1;
+  end
+
+  if( adc1_seq_cntr[7:0]>0 && adc1_seq_cntr[7:0]<33 && ~ADC1_SCLKOUT) begin
+    ADC1_SCLKOUT <= ~ADC1_SCLKOUT;
+    adc1_seq_cntr[7:0] <= adc1_seq_cntr[7:0] + 1'b1;
+  end
+
+  if( adc1_seq_cntr[7:0]==33 ) begin
+    ADC1_BUSY <= 0;
+    ADC1_SCLKOUT <= 0;
+    ADC1_SDOUT <= 0;
+    adc1_seq_cntr[7:0] <= 0;
+  end
+end
 
 endmodule
