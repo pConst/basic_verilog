6b99e1fdabb8e98f0df0ac2779e76990b0d3540a
Konstantin Pavlov <pavlovconst@gmail.com>
2019-10-02 13:40:27 +0300

    Added script to program Altera/Intel devices

---
+++ scripts/program_all.bat
@@ -0,0 +1,21 @@
+@echo off
+rem ------------------------------------------------------------------------------
+rem  program_all.bat
+rem  Konstantin Pavlov, pavlovconst@gmail.com
+rem ------------------------------------------------------------------------------
+
+echo "Listing programmers"
+C:\intelFPGA_lite\17.0\quartus\bin64\quartus_pgm -l
+
+echo "Programming first FPGA in the chain"
+C:\intelFPGA_lite\17.0\quartus\bin64\quartus_pgm -c ^
+ "USB-Blaster on 192.168.0.182 [USB-1]" -m jtag -o ^
+ "P;D:\project1\main.sof@1"
+
+echo "Programming second FPGA in the chain"
+C:\intelFPGA_lite\17.0\quartus\bin64\quartus_pgm -c ^
+ "USB-Blaster on 192.168.0.182 [USB-1]" -m jtag -o ^
+ "P;D:\project2\main.sof@2"
+
+pause
+exit
